406b372415f608c8412f535968cdafe2cf23dec4
balaji <balajiscientist1@gmail.com>
2025-05-23 08:05:58 +0530

    updated file path

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import streamlit as st
 from prediction_helper import predict
 
 # Define the page layout
-st.title('Apollo Health Insurance Cost Predictor')
+st.title('Apollo Health Insurance Cost premium Predictor')
 
 categorical_options = {
     'Gender': ['Male', 'Female'],
