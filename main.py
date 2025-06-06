403dc2e862df3d7dbdd65808a2709456108e1c46
balaji <balajiscientist1@gmail.com>
2025-06-06 22:31:23 +0530

    push streamilt app code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from prediction_helper import predict
 import base64
 
 # Set the page title
-st.title('Apollo Health Insurance Premium Predictor')
+st.title('Cdhpm Apollo Health Insurance Premium Predictor ')
 
 # Define categorical options
 categorical_options = {
